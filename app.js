3f43d417d7c2ca7528f437daa2a0befacf0d9c22
김진수 <nsa06035@gmail.com>
2023-07-25 15:38:22 +0900

    refactor : 쿼리문 room_id 값 uuid로 변경 및 석차조회 api 추가 및 이미지 업로드 api, 라운드별 게임시작 api 엔드포인트 변경

--- app.js
+++ app.js
@@ -7,20 +7,20 @@ import createRouter from './routes/createrooms'; // 사용자 방만들기
 import http from 'http';
 import { Server } from 'socket.io';
 import chat from './sockets/chat';
-import imageUpload from "./routes/imageUpload";
-import roundStart from "./routes/roundStart";
+import imageUpload from "./routes/imageUpload"; // 이미지 업로드하기
+import roundStart from "./routes/roundStart"; // 라운드별 게임 시작하기
 import joinRoomRouter from './routes/joinroom'; // 사용자 입장하기 
-
+import checkRanks from './routes/checkRanks'; // 석차조회하기
 
 const app = express();
 const port = 8080;
 
 app.use(cors());
 app.use(express.json());
-app.use(cors());
 
-app.use('/', imageUpload);
-app.use('/', roundStart);
+app.use('/', imageUpload);  // 이미지 업로드하기
+app.use('/', roundStart); // 라운드별 게임 시작하기
+app.use('/', checkRanks);  // 석차조회하기
 
 // db 연결
 const db = mysql.createConnection({
