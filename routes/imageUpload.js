b569a0918acfc2bf2077319e2fd9838480ae9026
ppinkypeach <mira20932@gmail.com>
2023-08-02 14:39:30 +0900

    fix : imageUpload api 수정

--- routes/imageUpload.js
+++ routes/imageUpload.js
@@ -1,30 +1,51 @@
 require('@babel/register');
 import express from 'express';
-import mysql from 'mysql2';
+import { Sequelize, DataTypes } from 'sequelize';
 import dotenv from 'dotenv';
 import multer from 'multer';
 import aws from 'aws-sdk';
 // import cors from 'cors';
 
 const router = express.Router();
-
 dotenv.config();
-// app.use(cors());
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+const sequelize = new Sequelize(
+    process.env.MYSQL_DATABASE,
+    process.env.MYSQL_USER,
+    process.env.MYSQL_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,  
+      dialect: 'mysql',
+      logging: false
+    }
+);
+
+const Room = sequelize.define('Room', {
+  uuid: { type: DataTypes.STRING, allowNull: false },
+  category_id: { type: DataTypes.INTEGER, allowNull: false },
+  time: { type: DataTypes.INTEGER, allowNull: false },
+  player_num: { type: DataTypes.INTEGER, allowNull: false }
 });
 
-db.connect((err) => {
-  if (err) {
-    throw err;
-  }
-});
 
 /* 게임 라운드별 그림 저장 API - 매 라운드마다 출제자가 그린 그림을 S3에 저장한다. */
+
+
+const Rounds = sequelize.define('Rounds', {
+  room_id: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  word_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  picture_url: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {});
 
 // 환경변수 설정
 const YOUR_S3_BUCKET_NAME = process.env.BUCKET_NAME;
@@ -44,11 +65,12 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 
-router.post('/api/v1/rooms/:room_id/picture/rounds', upload.single('image'), (req, res) => {
+router.post('/api/v1/rooms/:uuid/picture/:word_id/rounds', upload.single('image'), (req, res) => {
   
   const file = req.file;
   const fileContent = file.buffer;
-  const room_id = req.params.room_id;
+  const uuid = req.params.uuid;
+  const word_id = req.params.word_id;
   
   // S3에 이미지 업로드
   const params = {
@@ -68,25 +90,24 @@ router.post('/api/v1/rooms/:room_id/picture/rounds', upload.single('image'), (re
 
     // url 데이터베이스에 삽입(수정)
     const picture_url = data.Location;
-    console.log(picture_url);
-    // 1번 게임방, word_id가 1 이라고 가정
-    const sql = `UPDATE Rounds AS r1 JOIN ( 
-      SELECT MIN(id) AS min_id 
-      FROM Rounds 
-      WHERE room_id = (SELECT id FROM Rooms WHERE uuid = '${room_id}')
-      AND picture_url IS NULL 
-      ) AS r2 
-      ON r1.id = r2.min_id 
-      SET r1.picture_url = '${picture_url}' 
-      WHERE r1.room_id = (SELECT id FROM Rooms WHERE uuid = '${room_id}') 
-      AND r1.picture_url IS NULL;`;
-    db.query(sql, (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("데이터 삽입 성공!");
+
+    
+
+    async function insertRound() {
+      try {
+        const room = await Room.findOne({ where: {uuid: uuid} });
+        const result = await Rounds.create({
+          room_id : room.id,
+          word_id: word_id,
+          picture_url: picture_url,
+        });
+        console.log('Round inserted successfully:', result.toJSON());
+      } catch (error) {
+        console.error('Error inserting Round:', error);
       }
-    });
+    }
+    
+    insertRound();
   });
 });
 
