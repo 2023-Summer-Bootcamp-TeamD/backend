26b5ec78b0d573eaf7541306c7c0471cd17824d0
seunghwan <sun1238799@naver.com>
2023-08-02 15:19:21 +0900

    #feat: 새로운 기능과 관련된 것을 의미한다.
    #fix: 오류와 같은 것을 수정했을 때 사용한다.
    
    #docs: 문서와 관련하여 수정한 부분이 있을 때 사용한다.
    
    #style: 코드의 변화와 관련없는 포맷이나 세미콜론을 놓친 것과 같은 부분들을 의미한다.
    
    #refactor: 코드의 리팩토링을 의미한다.
    
    #test: test를 추가하거나 수정했을 때를 의미한다.
    
    #chore: build와 관련된 부분, 패키지 매니저 설정 등 여러가지 production code와 무관한 부분 들을 의미한다. 말 그대로 자질구레#한 일들. 초기 설정도 여기에 포함
    
    #design: css, ui 변경
    
    #rename: 폴더 이름, 경로 변경

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-require('@babel/register');
-import express from 'express';
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import createRouter from './routes/createRooms'; // 사용자 방만들기
-import http from 'http';
-import { Server } from 'socket.io';
-import chat from './sockets/chat';
+require("@babel/register");
+import express from "express";
+import mysql from "mysql2";
+import dotenv from "dotenv";
+import cors from "cors";
+import createRouter from "./routes/createRooms"; // 사용자 방만들기
+import http from "http";
+import { Server } from "socket.io";
+import chat from "./sockets/chat";
 import imageUpload from "./routes/imageUpload"; // 이미지 업로드하기
 import roundStart from "./routes/roundStart"; // 라운드별 게임 시작하기
-import joinRoomRouter from './routes/joinRoom'; // 사용자 입장하기 
-import CategoryRouter from './routes/pictureResults'; // 카데고리 별 그림 조회
-import checkRanks from './routes/checkRanks'; // 석차조회하기
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-
+import joinRoomRouter from "./routes/joinRoom"; // 사용자 입장하기
+import CategoryRouter from "./routes/pictureResults"; // 카데고리 별 그림 조회
+import checkRanks from "./routes/checkRanks"; // 석차조회하기
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 
 const app = express();
 const port = 8080;
-const swaggerDocument = YAML.load('./swagger/swagger.yaml');
+const swaggerDocument = YAML.load("./swagger/swagger.yaml");
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-app.use('/', imageUpload);  // 이미지 업로드하기
-// app.use('/', roundStart); // 라운드별 게임 시작하기
-app.use('/', checkRanks);  // 석차조회하기
+app.use("/", imageUpload); // 이미지 업로드하기
+app.use("/", roundStart); // 라운드별 게임 시작하기
+app.use("/", checkRanks); // 석차조회하기
 
 dotenv.config();
 
@@ -60,12 +61,12 @@ const roomsRouter = createRouter(db);
 app.use(roomsRouter); // 사용자 방만들기
 
 const joinRouter = joinRoomRouter(db);
-app.use(joinRouter); // 사용자 입장하기 
+app.use(joinRouter); // 사용자 입장하기
 
-const pictureRouter = CategoryRouter(db)
+const pictureRouter = CategoryRouter(db);
 app.use(pictureRouter); // 카테고리 별 그림 조회
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 server.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
