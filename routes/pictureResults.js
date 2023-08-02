3b434be7dab5e6d33602a97ed041b85eb942af27
ppinkypeach <mira20932@gmail.com>
2023-08-02 16:54:41 +0900

    refactor : pictureResults api endpoint와 반환값 리팩토링

--- routes/pictureResults.js
+++ routes/pictureResults.js
@@ -1,36 +1,82 @@
 import express from 'express';
-import mysql from 'mysql2';
-
+import { Sequelize, DataTypes } from 'sequelize';
 const router = express.Router();
 
-const CategoryRouter = (db) => {
-    // API endpoint
-    router.get('/api/v1/results/category/:category_id', (req, res) => {
+const sequelize = new Sequelize(
+    process.env.MYSQL_DATABASE,
+    process.env.MYSQL_USER,
+    process.env.MYSQL_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,  
+      dialect: 'mysql',
+      logging: false
+    }
+);
+
+const Rounds = sequelize.define('Rounds', {
+    room_id: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    word_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    picture_url: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+}, {});
+
+
+
+const Words = sequelize.define('Words', {
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Categories',
+        key: 'id'
+      }
+    },
+    word: {
+      type: DataTypes.STRING,
+      allowNull: true 
+    }
+}, {});
+
+Words.hasOne(Rounds, { foreignKey: 'word_id' });
+Rounds.belongsTo(Words, { foreignKey: 'word_id' });
+
+
+const CategoryRouter = () => {
+    router.get('/api/v1/results/categories/:category_id/words', async (req, res) => {
         const categoryId = req.params.category_id;
-        const wordToFilter = req.query.word; // Get the word filter parameter value
+        const wordToFilter = req.query.word;
 
-        // Build the query to filter by category_id and word (if provided)
-        let query = `
-            SELECT Words.word, Rounds.picture_url
-            FROM Words
-            INNER JOIN Rounds ON Words.id = Rounds.word_id
-            WHERE Words.category_id = ?
-        `;
-        const queryParams = [categoryId];
-
-        if (wordToFilter) {
-            query += ' AND Words.word = ?';
-            queryParams.push(wordToFilter);
-        }
-
-        db.query(query, queryParams, (error, results) => {
-            if (error) {
-                console.error('Error executing query:', error);
-                res.status(500).json({ error: 'An error occurred', specificError: error.message });
-            } else {
-                res.status(200).json(results);
+        try {
+            const whereOptions = { category_id: categoryId };
+            if (wordToFilter) {
+                whereOptions.word = wordToFilter;
             }
-        });
+
+            const wordsWithPictures = await Words.findAll({
+                where: whereOptions,
+                attributes: ['id', 'category_id', 'word'],
+                include: [{
+                    model: Rounds,
+                    as: 'Round',
+                    attributes: ['picture_url']
+                }],
+                raw: true
+            });
+
+            res.status(200).json(wordsWithPictures);
+        } catch (error) {
+            console.error('Error executing query:', error);
+            res.status(500).json({ error: 'An error occurred', specificError: error.message });
+        }
     });
 
     return router;
